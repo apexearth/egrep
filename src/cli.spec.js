68c34b45d61382902ba7fb6319c34fdddbaf33da
Christopher Jacobs <apexearth@gmail.com>
2019-01-24 18:29:13 -0800

    Create cli.spec.js

---
+++ src/cli.spec.js
@@ -0,0 +1,6 @@
+const {expect} = require('chai')
+
+describe('cli', () => {
+    it.skip('test cli functionality', () => expect(1).to.equal(0))
+    it.skip('compare with classic grep functionality')
+})
